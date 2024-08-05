b3b7c0a04a6ddf37ab1de0c8a2871afb4c635a6c
Danu Oprea <danop-dev@users.noreply.github.com>
2024-08-05 13:58:39 +0300

    Confg Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -6,6 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  
+  
+  const config = new DocumentBuilder()
+    .setTitle('App API')
+    .setDescription('The App API description')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
